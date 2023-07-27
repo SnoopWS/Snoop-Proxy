00b6a90843963bf2f1b884943fa5caf10534f09a
Snoop <123210023+SnoopWS@users.noreply.github.com>
2023-07-28 05:57:34 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,26 +6,10 @@ import (
 	"goProxy/core/server"
 	"io"
 	"log"
-	"net"
 	"os"
 )
 
-var ipWhitelist = []string{
-	"176.97.210.166",
-	"87.237.52.211",
-}
-
 func main() {
-
-	// Check if the local IP is whitelisted
-	localIP, err := getLocalIP()
-	if err != nil {
-		log.Fatalf("Failed to get local IP: %v", err)
-	}
-
-	if !isWhitelisted(localIP) {
-		log.Fatalf("Local IP %s is not whitelisted. Exiting.", localIP)
-	}
 
 	logFile, err := os.OpenFile("crash.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
@@ -47,25 +31,4 @@ func main() {
 
 	//Keep server running
 	select {}
-}
-
-func isWhitelisted(ip string) bool {
-	for _, whitelistedIP := range ipWhitelist {
-		if ip == whitelistedIP {
-			return true
-		}
-	}
-	return false
-}
-
-func getLocalIP() (string, error) {
-	conn, err := net.Dial("udp", "8.8.8.8:80")
-	if err != nil {
-		return "", err
-	}
-	defer conn.Close()
-
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
-
-	return localAddr.IP.String(), nil
 }
